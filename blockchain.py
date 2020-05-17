466c366debcb31ed7699ef46a699f47a4421f272
Tom (Surface) <t.austinbennett@gmail.com>
2020-05-16 21:50:05 -0400

    Finish the blockchain class

--- blockchain.py
+++ blockchain.py
@@ -2,14 +2,39 @@
 See https://hackernoon.com/learn-blockchains-by-building-one-117428612f46 for tutorial.
 '''
 
+import hashlib
+import json
+from time import time
+
 class Blockchain(object):
     def __init__(self):
         self.chain = []
         self.current_transactions = []
 
-    def new_block(self):
-        # Creates a new block and adds it to the chain
-        pass
+        # create the genesis block
+        self.new_block(previous_hash=1,proof=100)
+
+    def new_block(self, proof, previous_hash=None):
+        """
+        Create a new Block in the Blockchain
+        :param proof: <int> The proof given by the Proof of Work algorithm
+        :param previous_hash: (Optional) <str> Hash of previous Block
+        :return: <dict> New Block
+        """
+
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1]),
+        }
+
+        # reset the current list of transactions
+        self.current_transactions = []
+
+        self.chain.append(block)
+        return block
 
     def new_transaction(self, sender, recipient, amount):
         """
@@ -30,12 +55,18 @@ class Blockchain(object):
     # staticmethod cannot change the properties of the class instance
     @staticmethod
     def hash(block):
-        # Hashes a block
-        pass
+        """
+        Creates a SHA-256 hash of a Block
+        :param block: <dict> Block
+        :return: <str>
+        """
+
+        # the dictionary must be ordered or we'll have inconsistent hashes
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
 
     # property treats this function like a property of the class
     # check out https://www.freecodecamp.org/news/python-property-decorator/ for more
     @property
     def last_block(self):
-        # Returns the last block in the chain
-        pass
+        return self.chain[-1]
