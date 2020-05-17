f120e766c4089432a9820618f57fa98ba41a98a5
Tom (Surface) <t.austinbennett@gmail.com>
2020-05-16 21:33:13 -0400

    Example block, new_transaction

--- blockchain.py
+++ blockchain.py
@@ -6,3 +6,36 @@ class Blockchain(object):
     def __init__(self):
         self.chain = []
         self.current_transactions = []
+
+    def new_block(self):
+        # Creates a new block and adds it to the chain
+        pass
+
+    def new_transaction(self, sender, recipient, amount):
+        """
+        Creates a new transaction to go into the next mined Block
+        :param sender: <str> Address of the Sender
+        :param recipient: <str> Address of the Recipient
+        :param amount: <int> Amount
+        :return: <int> The index of the Block that will hold this transaction
+        """
+        self.current_transactions.append({
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount,
+        })
+
+        return self.last_block['index'] + 1
+
+    # staticmethod cannot change the properties of the class instance
+    @staticmethod
+    def hash(block):
+        # Hashes a block
+        pass
+
+    # property treats this function like a property of the class
+    # check out https://www.freecodecamp.org/news/python-property-decorator/ for more
+    @property
+    def last_block(self):
+        # Returns the last block in the chain
+        pass
